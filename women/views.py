5aed525273e7aa74696c007019976189b812ea60
Mottshi <mxttshi@gmail.com>
2024-11-07 00:12:25 +0300

    Create first urls

--- women/views.py
+++ women/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def index(request) -> HttpResponse:
+    return HttpResponse("Страница приложения women.")
+
+def categories(request) -> HttpResponse:
+    return HttpResponse("<h1>Статьи по категориям</h1>")
+
+
