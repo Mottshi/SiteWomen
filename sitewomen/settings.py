6a58eca9d3fff6928b68396a4d2a262e8cb24f16
Admin <mxttshi@gmail.com>
2024-11-16 01:32:51 +0300

    Http404 response

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f8^mt6)6e2i!w*3bz@f#g=qf1_9v(^sm7$3r^g0(*lv*kt=sm)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 APPEND_SLASH = False
 
 # Application definition
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 
 
 # Create your views here.
@@ -12,8 +12,16 @@ def categories(request, cat_id: int) -> HttpResponse:
 
 
 def categories_by_slug(request, cat_slug: str) -> HttpResponse:
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
 
 
 def archive(request, year: int) -> HttpResponse:
+    if year > 2024:
+        raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1><p>year:{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
