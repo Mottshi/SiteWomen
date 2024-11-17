c3961ac04358bd6a09eca20bc5a9362faa78b862
Mottshi <mxttshi@gmail.com>
2024-11-18 00:06:28 +0300

    template tags

--- women/views.py
+++ women/views.py
@@ -2,27 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
-
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [{'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+           {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+           {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': False}, ]
 
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    # t = render_to_string("women/index.html")
     data = {
         "title": "Главная страница",
         "menu": menu,
-        "str": "lol",
-        "float": 1.235,
-        "set": {1, 2, 3, 4, 5},
-        "dict": {"key_1": "value_1", "key_2": "value_2"},
-        "obj": MyClass(10, 12)
+        "posts": data_db,
     }
     return render(request, "women/index.html", context=data)
 
