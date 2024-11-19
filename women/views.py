3320898ef5a8c85320adcc186f63c5120b276da1
Admin <mxttshi@gmail.com>
2024-11-19 23:08:32 +0300

    Make dynamic url

--- women/views.py
+++ women/views.py
@@ -4,11 +4,15 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 from django.template.defaultfilters import slugify
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 data_db = [{'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
-           {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-           {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': False}, ]
+           {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': True},
+           {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True}, ]
 
 
 # Create your views here.
@@ -21,23 +25,6 @@ def index(request) -> HttpResponse:
     return render(request, "women/index.html", context=data)
 
 
-def categories(request, cat_id: int) -> HttpResponse:
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id:{cat_id}</p>")
-
-
-def categories_by_slug(request, cat_slug: str) -> HttpResponse:
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
-
-
-def archive(request, year: int) -> HttpResponse:
-    if year > 2024:
-        uri = reverse("cats", args=("music",))
-        return redirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>year:{year}</p>")
-
-
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
@@ -45,3 +32,21 @@ def page_not_found(request, exception):
 def about(request):
     data = {"title": "О сайте"}
     return render(request, "women/about.html", context=data)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Статья с номер айди {post_id}")
+
+
+def addpage(request):
+    return HttpResponse(f"Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse(f"Обратная связь")
+
+
+def login(request):
+    return HttpResponse(f"Авторизация")
+
+
