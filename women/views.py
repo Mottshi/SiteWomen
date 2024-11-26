812a68cc6f8554d3235a07de8c5362794d7c41fa
Mottshi <mxttshi@gmail.com>
2024-11-26 22:59:47 +0300

    Created user manager and class Status

--- women/views.py
+++ women/views.py
@@ -32,7 +32,7 @@ cats_db = [
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    posts = Women.objects.filter(is_published=1)
+    posts = Women.published.all()
     data = {
         "title": "Главная страница",
         "menu": menu,
