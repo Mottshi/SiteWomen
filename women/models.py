d1ba1de120e024df66f86a48c6079c4b0acbe0fb
Mottshi <mxttshi@gmail.com>
2024-11-28 19:06:27 +0300

    Created tags

--- women/models.py
+++ women/models.py
@@ -20,6 +20,7 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=Status.DRAFT, choices=Status)
     cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts")
+    tags = models.ManyToManyField("TagPost", related_name="women", blank=True)
 
     objects = models.Manager()
     published = PublishedManager()
@@ -46,3 +47,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -1,7 +1,7 @@
 from django import template
 import women.views as views
 
-from women.models import Category
+from women.models import Category, TagPost
 
 register = template.Library()
 
@@ -10,3 +10,12 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {"cats": cats, "cat_selected": cat_selected}
+
+
+@register.inclusion_tag("women/list_tag.html")
+def show_tags(post=None):
+    if post:
+        tags = post.tags.all()
+    else:
+        tags = TagPost.objects.all()
+    return {"tags": tags}
