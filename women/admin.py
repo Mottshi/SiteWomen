b2523b2ebdc305e834ec1de37f68e7c7bc17cecb
Mottshi <mxttshi@gmail.com>
2024-12-02 23:42:52 +0300

    Create super user and admin models

--- women/admin.py
+++ women/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models.functions import Length
+
+from .models import Women, Category
+
 
 # Register your models here.
+
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ("title", "time_create", "is_published", "cat", "brief_info")
+    list_display_links = ("title", )
+    ordering = ("time_create", "title")
+    list_editable = ("is_published",)
+    list_per_page = 5
+    actions = ["set_published", "set_draft"]
+
+    @admin.display(description="Краткое описание", ordering=Length("content"))
+    def brief_info(self, women: Women):
+        return f"Описание {len(women.content)} символов"
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Измененно {count} записей")
+
+    @admin.action(description="Снять выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"Измененно {count} записей", messages.WARNING)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+
+
+admin.site.site_header = 'Панель администрирования'
+admin.site.index_title = 'Известные женщины мира'
