8b9be63cb13eabaf5f650488e79d6d40383a7c0d
Mottshi <mxttshi@gmail.com>
2024-12-03 19:57:44 +0300

    Created custom filters for admin

--- women/admin.py
+++ women/admin.py
@@ -5,6 +5,26 @@ from .models import Women, Category
 
 
 # Register your models here.
+#Кастомный фильтр
+class MarriedFilter(admin.SimpleListFilter):
+    title = "Статус женщин"
+    parameter_name = "status"
+
+
+    #то что на отображается указывает на первом индексе, то что отправляется на сервер на втором
+    def lookups(self, request, model_admin):
+        return [
+            ("married", "Замужем"),
+            ("single", "Не замужем"),
+        ]
+
+    #то, как фильто будет работать на queryset и что по итогу вернет
+    def queryset(self, request, queryset):
+        match self.value():
+            case "married":
+                return queryset.filter(husband__isnull=False)
+            case "single":
+                return queryset.filter(husband__isnull=True)
 
 
 @admin.register(Women)
@@ -15,11 +35,17 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ("is_published",)
     list_per_page = 5
     actions = ["set_published", "set_draft"]
+    search_fields = ("title", "cat__name")
+    list_filter = ("is_published", "cat__name", MarriedFilter) #добавление фильтров
 
+
+    #добавляет новое поле
     @admin.display(description="Краткое описание", ordering=Length("content"))
     def brief_info(self, women: Women):
         return f"Описание {len(women.content)} символов"
 
+
+    #добавляет новое дествией над таблицей
     @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
         count = queryset.update(is_published=Women.Status.PUBLISHED)
@@ -29,6 +55,9 @@ class WomenAdmin(admin.ModelAdmin):
     def set_draft(self, request, queryset):
         count = queryset.update(is_published=Women.Status.DRAFT)
         self.message_user(request, f"Измененно {count} записей", messages.WARNING)
+
+
+
 
 
 @admin.register(Category)
