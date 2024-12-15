c9d2b3eb918bfa7f26bbdff764867e744e6130ce
Mottshi <mxttshi@gmail.com>
2024-12-15 23:49:30 +0300

    Ended form topic

--- sitewomen/urls.py
+++ sitewomen/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from women.views import page_not_found
 
 urlpatterns = [
@@ -26,3 +27,6 @@ urlpatterns = [
 ]
 
 handler404 = page_not_found
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
